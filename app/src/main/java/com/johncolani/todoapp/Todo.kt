9101f16dc331a9b00b886e75bd3c0e0a5d6b12a8
johnhabibpour <johnhcolani@gmail.com>
2024-06-01 14:42:39 -0400

    init commit

---
+++ app/src/main/java/com/johncolani/todoapp/Todo.kt
@@ -0,0 +1,19 @@
+package com.johncolani.todoapp
+
+import java.time.Instant
+import java.util.Date
+
+data class Todo(
+    var id:Int,
+    var title : String,
+    var createdAt : Date
+)
+fun getFakeTodo(): List<Todo>{
+    return listOf<Todo>(
+        Todo(1,"First todo",Date.from(Instant.now())),
+        Todo(2,"Second todo",Date.from(Instant.now())),
+        Todo(2,"This is my third todo",Date.from(Instant.now())),
+        Todo(4,"this will be my forth todo",Date.from(Instant.now())),
+        Todo(5,"Fifth todo",Date.from(Instant.now())),
+    )
+}
